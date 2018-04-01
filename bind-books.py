cde84c135e167af81fb3edf65d1d530955ca3499
frimerke <frimerke@gmail.com>
2018-04-01 08:37:58 +0200

    added bind-books.py, only comments for now

---
+++ bind-books.py
@@ -0,0 +1,25 @@
+#
+# bind-books.py
+# a script to initialize a book/document into a collection.
+# loosely based on the node.js generate package script.
+#
+
+# Take arguments - primarily a file to work off of.
+
+# Pose questions.
+
+# Generate XML
+# validate against dtd
+
+# Create folders & XML file.
+#
+# /Library/<year-author-title>/
+#   ./src/<original-file>.pdf
+#   <year-author-title>.XML
+#
+
+# Move source file to src dir above.
+
+# Hook into database?
+
+# Exit gracefully.
